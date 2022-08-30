b912f243adeba8565cc0c6ee64ba0315309b8471
Chibuzor Obiora <218098562@student.uj.ac.za>
2022-08-31 01:20:12 +0200

    Create transportFee.js

---
+++ transportFee.js
@@ -0,0 +1,9 @@
+function transportFee (shift) {
+    if (shift.startsWith('morning')) {
+      return 'R20';
+  }   else if (shift.startsWith('afternoon')) {
+      return 'R10';
+      }   else if (shift.startsWith('nightshift')) {
+      return 'free';
+      }
+  }
